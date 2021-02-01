d76e44dde7684e441c9239cb2fd6788ce9e2d75e
andres <andcarv89@gmail.com>
2021-01-31 22:12:03 -0300

    cambio metodo client total debt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,9 @@ module ApplicationHelper
         total_debt = 0
         paid_out = client.paid_out
         client.services.each do |service|
-            total_debt += service.bill - paid_out
+            if service != nil
+                total_debt += service.bill - paid_out
+            end
         end
         return total_debt.round
     end
